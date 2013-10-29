ad9afacac9f6cca0f59a17c04fa40c220e51e397
Flea <andriusfl@gmail.com>
2013-10-29 22:34:12 +0200

    Initial version

---
+++ Achiles.Codex.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Achiles.Codex.Web.Startup))]
+namespace Achiles.Codex.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
