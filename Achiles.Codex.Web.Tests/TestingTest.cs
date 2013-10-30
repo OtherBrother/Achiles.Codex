4132699ec011ccb8cd854ec50eec0b35e5c3e96c
Flea <andriusfl@gmail.com>
2013-10-30 18:18:25 +0200

    Created empty test project

---
+++ Achiles.Codex.Web.Tests/TestingTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Achiles.Codex.Web.Tests
+{
+    [TestClass]
+    public class TestingTest
+    {
+        [TestMethod]
+        public void test_works()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
