ebd03e5ad6ceef4c11e6f1121ad27237af4bbe7b
Anbut <andbut@gmail.com>
2013-11-28 19:57:18 +0200

    New view files made for creating NGCEquipment items

---
+++ Achiles.Codex.Web/Controllers/NCGEquipmentItemController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Achiles.Codex.Web.Controllers
+{
+    public class NCGEquipmentItemController : CodexItemBaseController
+    {
+        //
+        // GET: /NCGEquipmentItem/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+
+
+
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Edit(CodexItemModel<NcgEquipmentItem> input)
+        {
+
+            if (ModelState.IsValid)
+            {
+                //insert or update properties common for all base codex items
+                var storedItem = UpsertBaseCodexItem(input);
+
+                //set item other properties if neccessary..
+                storedItem.Gear = input.CodexItem.Gear;
+
+                //..and save 
+                DocumentSession.SaveChanges();
+                Success("New equipment is created");
+            }
+
+            return View(input);
+        }
+
+
+
+}
