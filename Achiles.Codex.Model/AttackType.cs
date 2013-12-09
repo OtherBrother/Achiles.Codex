d66c3a3d3f92c43892de733e5d8774e1fb45e485
Flea <andriusfl@gmail.com>
2013-12-09 20:36:37 +0200

    Model update

---
+++ Achiles.Codex.Model/AttackType.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Achilles.Codex.Model
+{
+    public class AttackType
+    {
+        public AttackType()
+        {
+            Effects = new List<Effect>();
+        }
+        
+        public string Type { get; set; }
+        public int DamageValue { get; set; }
+        public int ArmorPiercingModifier { get; set; }
+        public List<Effect> Effects { get; set; }
+    }
+}
---
+++ Achiles.Codex.Model/Weapon.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Achilles.Codex.Model
+{
+    public class Weapon : EquipmentItem
+    {
+        public Weapon()
+        {
+            Properties = new List<string>();
+            AttackTypes = new List<AttackType>();
+        }
+
+        public int Reach { get; set; }
+        public int MinimumStrenght { get; set; }
+        
+        /// <summary>
+        /// Two handed, consealable, +1 against shields...
+        /// </summary>
+        public List<string> Properties { get; set; }
+        public List<AttackType> AttackTypes { get; set; }
+        
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendFormat("W:{0}", Weight)
+                .AppendLine()
+                .AppendFormat("R:{0}", Reach)
+                .AppendLine()
+                .AppendFormat("St:{0}", MinimumStrenght)
+                .AppendLine();
+
+            foreach (var a in AttackTypes)
+            {
+                sb.AppendFormat("{0} {1}+{2}", a.Type, a.DamageValue, a.ArmorPiercingModifier).AppendLine().Append("Effects:");
+                sb.AppendLine(string.Join("/", a.Effects.Select(e => string.Format("{0}{1}", e.Span, e.EffectType.Substring(0, 1)))));
+
+            }
+            sb.AppendLine();
+            foreach (var p in Properties)
+            {
+                sb.AppendFormat("{0}", p).AppendLine();
+            }
+            return sb.ToString();
+        }
+
+    }
+
+
+}
