9e12f502d751c0815286b0c709defa44d95e53d4
Arkenep <nim@nim.lt>
2013-12-12 16:39:39 +0200

    Tried to create partial view for EquipmentItem. Need help with how models work...

---
+++ Achiles.Codex.Web/Controllers/EquipmentItemController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using Achilles.Codex.Model;
+using Achilles.Codex.Web;
+using Achilles.Codex.Web.Models;
+
+namespace Achilles.Codex.Web.Controllers
+{
+    public class EquipmentItemController : CodexItemBaseController
+    {
+        
+
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Edit(CodexItemModel<EquipmentItem> input)
+        {
+            if (ModelState.IsValid)
+            {
+                //insert or update properties common for all base codex items
+                var storedItem = UpsertBaseCodexItem(input);
+
+                storedItem.Price = input.CodexItem.Price;
+                storedItem.Weight = input.CodexItem.Weight;
+                //..and save 
+
+                DocumentSession.SaveChanges();
+                Success("New equipment item is created");
+            }
+            return View(input);
+        }
+
+
+    }
+}
---
+++ Achiles.Codex.Web/Controllers/MiscellaneousItemController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using Achilles.Codex.Model;
+using Achilles.Codex.Web;
+using Achilles.Codex.Web.Models;
+
+namespace Achilles.Codex.Web.Controllers
+{
+    public class MiscellaneousItemController : EquipmentItemController
+    {
+        //
+        // GET: /MiscellaneousItem/
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var model = GetModel<MiscellaneousItem>(id);
+            return View(model);
+        }
+
+
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Edit(CodexItemModel<MiscellaneousItem> input)
+        {
+            if (ModelState.IsValid)
+            {
+                //insert or update properties common for all base codex items
+                var storedItem = UpsertBaseCodexItem(input);
+                //..and save 
+                DocumentSession.SaveChanges();
+                Success("New miscellaneous item is created");
+            }
+            return View(input);
+        }
+
+
+    }
+}
