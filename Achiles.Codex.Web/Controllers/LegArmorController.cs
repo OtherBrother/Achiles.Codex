a0e4768ba3b40ad1f744a851e090697422e5029d
Arkenep <nim@nim.lt>
2013-12-12 19:44:04 +0200

    Added LegArmor, ArmArmor, BodyArmor, HeadArmor forms.

---
+++ Achiles.Codex.Web/Controllers/LegArmorController.cs
@@ -0,0 +1,42 @@
+﻿using System.Web.Mvc;
+using Achilles.Codex.Model;
+using Achilles.Codex.Web;
+using Achilles.Codex.Web.Models;
+
+namespace Achilles.Codex.Web.Controllers
+{
+    public class LegArmorController : CodexItemBaseController
+    {
+        //
+        // GET: /LegArmor/
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        public ActionResult Edit(string id)
+        {
+            var model = GetModel<LegArmor>(id);
+            return View(model);
+        }
+
+
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Edit(CodexItemModel<LegArmor> input)
+        {
+            if (ModelState.IsValid)
+            {
+                //insert or update properties common for all base codex items
+                var storedItem = UpsertBaseCodexItem(input);
+                //..and save 
+                DocumentSession.SaveChanges();
+                Success("New leg armor is created");
+            }
+            return View(input);
+        }
+
+
+    }
+}
