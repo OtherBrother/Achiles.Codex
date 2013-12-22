37c4c851aa559a55211fcf841f54c7ab4aaa428b
Anbut <andbut@gmail.com>
2013-12-22 16:55:51 +0200

    Created Rule/Index

---
+++ Achiles.Codex.Web/Controllers/RuleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using Achilles.Codex.Model;
+using Achilles.Codex.Web;
+using Achilles.Codex.Web.Models;
+
+
+namespace Achilles.Codex.Web.Controllers
+{
+    public class RuleController : CodexItemBaseController
+    {
+        //
+        // GET: /Rule/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
