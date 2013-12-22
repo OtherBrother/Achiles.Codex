4f1ff51ba27ae61700365f786aba20cbb2ee8430
Anbut <andbut@gmail.com>
2013-12-22 17:19:10 +0200

    Created Rule creation/edit form

--- Achiles.Codex.Web/Controllers/RuleController.cs
+++ Achiles.Codex.Web/Controllers/RuleController.cs
@@ -14,5 +14,25 @@ namespace Achilles.Codex.Web.Controllers
         {
             return View();
         }
+        public ActionResult Edit(string id)
+        {
+            var model = GetModel<Rule>(id);
+            return View(model);
+        }
+        [ValidateInput(false)]
+        [HttpPost]
+        public ActionResult Edit(CodexItemModel<Rule> input)
+        {
+            if (ModelState.IsValid)
+            {
+                //insert or update properties common for all base codex items
+                var storedItem = UpsertBaseCodexItem(input);
+                //..and save 
+                DocumentSession.SaveChanges();
+                Success("New rule item is created");
+            }
+            return View(input);
+        }
+
 	}
 }
