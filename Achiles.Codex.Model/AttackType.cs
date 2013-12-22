eb425d6dbe97f873cdb526e551cd3edce3c46b33
Flea <andriusfl@gmail.com>
2013-12-22 17:07:47 +0200

    Weapon list and form fixes

--- Achiles.Codex.Model/AttackType.cs
+++ Achiles.Codex.Model/AttackType.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace Achilles.Codex.Model
 {
@@ -13,5 +14,14 @@ namespace Achilles.Codex.Model
         public int DamageValue { get; set; }
         public int ArmorPiercingModifier { get; set; }
         public List<Effect> Effects { get; set; }
+        public override string ToString()
+        {
+            var x = new StringBuilder();
+            foreach (var e in Effects)
+            {
+                x.AppendFormat("{0}{1} ", e.EffectType[0], e.Span);
+            }
+            return x.ToString();
+        }
     }
 }
--- Achiles.Codex.Web/Scripts/codex-item/edit-codex-item-article.js
+++ Achiles.Codex.Web/Scripts/codex-item/edit-codex-item-article.js
@@ -1,1 +1,8 @@
-﻿tinymce.init({ selector: '#ArticleBody', entity_encoding: "raw" });
+﻿tinymce.init({
+    selector: '#ArticleBody',
+    entity_encoding: "raw",
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code | table | preview",
+    plugins: ["code", "image", "table", "preview"],
+    paste_as_text: true
+});
+
